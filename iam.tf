634b21c3b0be98694ea4a82e4772a21261b4f6f9
sqautboy <migonyoung01@gmail.com>
2025-06-23 20:46:05 +0900

    fix: add eip connect IAM policy

--- iam.tf
+++ iam.tf
@@ -16,6 +16,22 @@ resource "aws_iam_role_policy_attachment" "ec2_ssm" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+# EC2 인스턴스가 EIP를 연결할 수 있도록 정책 추가
+resource "aws_iam_role_policy" "ec2_eip_policy" {
+  name = "ec2-associate-eip-policy"
+  role = aws_iam_role.ec2_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = "ec2:AssociateAddress"
+        Effect   = "Allow"
+        Resource = "*" # 실제 환경에서는 특정 EIP ARN으로 제한하는 것이 좋습니다.
+      },
+    ]
+  })
+}
+
 resource "aws_iam_instance_profile" "ec2" {
   name = "spot-app-instance-profile"
   role = aws_iam_role.ec2_role.name
